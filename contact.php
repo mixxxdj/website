fe43271ff5cbb79df3c12bc0d9b9bcc15d745e6f
RJ Ryan <rryan@mixxx.org>
2011-11-03 22:44:42 -0400

    Update contributor list for 1.10.0.

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
     <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
     <meta name="author" content="haran" />
     <meta name="generator" content="haran" />
-
+    
     <link rel="stylesheet" type="text/css" href="./css/prosimii-screen.css" media="screen, tv, projection" title="Default" />
     <link rel="stylesheet alternative" type="text/css" href="./css/prosimii-print.css" media="screen" title="Print Preview" />
     <link rel="stylesheet" type="text/css" href="./css/prosimii-print.css" media="print" />
@@ -15,159 +15,227 @@
     <![endif]-->
     <title>Mixxx | Contact</title>
   </head>
-
+  
   <body>
     <!-- For non-visual user agents: -->
-      <div id="top"><a href="#main-copy" class="doNotDisplay doNotPrint">Skip to main content.</a></div>
+    <div id="top"><a href="#main-copy" class="doNotDisplay doNotPrint">Skip to main content.</a></div>
 
     <!-- ##### Header ##### -->
-	<?php  include 'header.php';?>
+    <?php  include 'header.php';?>
 
     <!-- ##### Main Copy ##### -->
     <div id="main-copy">
-    <div class="rowOfBoxes features">
+      <div class="rowOfBoxes features">
         <div class="fullWidth noBorderOnLeft">   
-		        <h1>Contact Us</h1>
-		        <p>Support for <b>users</b> of Mixxx is available through our <a href="/forums">community forums</a> and our IRC channel as described on our <a href="support.php">Support page</a>.
+	  <h1>Contact Us</h1>
+	  <p>
+	    Support for <b>users</b> of Mixxx is available through
+	    our <a href="/forums">community forums</a> and our IRC channel as
+	    described on our <a href="support.php">Support page</a>.
+	    
+	    If you think you've found a bug, please file a bug report in
+	    our <a href="https://bugs.launchpad.net/mixxx/">bug
+	    tracker</a>.
+	  </p>
 				
-				If you think you've found a bug, please file a bug report in our <a href="https://bugs.launchpad.net/mixxx/">bug tracker</a>.</p>
-				
-			<p><b>Developers</b> working on Mixxx collaborate with each other via the <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">mixxx-devel</a> mailing list and our IRC channel (#mixxx on Freenode). To send an email to the list, you must <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">subscribe first</a>. The list is low traffic.</p>
-			<p>If you're a <b>DJ hardware manufacturer</b> and would like your product to work with Mixxx, please contact our Project Manager, <a href="http://www.hep.ucl.ac.uk/~adamd">Adam Davison</a>. We accept donations of DJ equipment which we use to improve both specific and general hardware support in Mixxx.</p>	
+	  <p>
+	    <b>Developers</b> working on Mixxx collaborate with each other via
+	    the <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">mixxx-devel</a>
+	    mailing list and our IRC channel (#mixxx on Freenode). To send an
+	    email to the list, you
+	    must <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">subscribe
+	    first</a>. The list is low traffic.
+	  </p>
+
+	  <p>
+	    If you're a <b>DJ hardware manufacturer</b> and would like your
+	    product to work with Mixxx, please contact our Project
+	    Manager, <a href="http://www.hep.ucl.ac.uk/~adamd">Adam
+	    Davison</a>. We accept donations of DJ equipment which we use to
+	    improve both specific and general hardware support in Mixxx.
+	  </p>
 			
-			<p><b>For other inquiries</b>, please contact <a href="http://www.hep.ucl.ac.uk/~adamd">Adam Davison</a>.</p>
+	  <p>
+	    <b>For other inquiries</b>, please
+	    contact <a href="http://www.hep.ucl.ac.uk/~adamd">Adam
+	    Davison</a>.
+	  </p>
 			   
-			<h2>Credits</h2>
-			<p>In 2001, Mixxx was started by <a href="http://www.nbi.dk/~kenand/">Ken</a> and <a href="http://haste.dk/tue/">Tue</a> Haste Andersen. Today, development is done by an open source development team from around the world, led by Adam Davison (Project Manager) and Albert Santoni (Lead Developer):
-    <p class="credits"><b>Mixxx 1.8.0 Development Team</b></p>
-<p class="credits">
-<a href="http://www.hep.ucl.ac.uk/~adamd">Adam Davison</a><br>
-<a href="http://www.santoni.ca/albert">Albert Santoni</a><br>
-<a href="http://www.rustyryan.net">RJ Ryan</a><br>
-<a href="http://stacktrace.org/">Garth Dahlstrom</a><br>
-<a href="http://www.seanpappalardo.com">Sean Pappalardo</a><br>
-Phillip Whelan<br>
-Tobias Rafreider<br>
-S. Brandt<br>
-</p>
+	  <h2>Credits</h2>
+	  <p>
+	    In 2001, Mixxx was started
+	    by <a href="http://www.nbi.dk/~kenand/">Ken</a>
+	    and <a href="http://haste.dk/tue/">Tue</a> Haste Andersen. Today,
+	    development is done by an open source development team from around
+	    the world, led by Adam Davison (Project Manager) and RJ Ryan (Lead
+	    Developer):
+	  </p>
 
-<p class="credits"><b>With contributions from:</b></p>
-<p class="credits">
-Mark Hills<br>
-Andre Roth<br>
-Robin Sheat<br>
-Mark Glines<br>
-Mathieu Rene<br>
-Miko Kiiski<br>
-Brian Jackson<br>
-Owen Williams<br>
-Andreas Pflug<br>
-Bas van Schaik<br>
-J&aacute;n Jockusch<br>
-Oliver St&ouml;neberg<br>
-Jan Jockusch<br>
-C. Stewart<br>
-Bill Egert<br>
-Zach Shutters<br>
-Owen Bullock<br>
-Bill Good<br>
-Graeme Mathieson<br>
-Sebastian Actist<br>
-Jussi Sainio<br>
-David Gnedt<br>
-Antonio Passamani<br>
-</p>
+	  <p class="credits"><b>Mixxx 1.10.0 Development Team</b></p>
 
-<p class="credits"><b>And special thanks to:</b></p>
-<p class="credits">
-<a href="http://www.stantondj.com">Stanton</a><br>
-<a href="http://www.hercules.com">Hercules</a><br>
-<a href="http://www.echoaudio.com">Echo Digital Audio</a><br>
-<a href="http://www.eks.fi">EKS</a><br>
-<a href="http://code.google.com/soc">Google Summer of Code</a><br>
-Adam Bellinson<br>
-Alexandre Bancel<br>
-Melanie Thielker<br>
-Julien Rosener<br>
-Pau Arum&iacute;<br>
-David Garcia<br>
-Seb Ruiz<br>
-Joseph Mattiello<br>
-</p>
+	  <p class="credits">
+	    <a href="http://www.hep.ucl.ac.uk/~adamd">Adam Davison</a><br/>
+	    <a href="http://www.santoni.ca/albert">Albert Santoni</a><br/>
+	    <a href="http://www.rustyryan.net">RJ Ryan</a><br/>
+	    <a href="http://stacktrace.org/">Garth Dahlstrom</a><br/>
+	    <a href="http://www.seanpappalardo.com">Sean Pappalardo</a><br/>
+	    Phillip Whelan<br/>
+	    Tobias Rafreider<br/>
+	    S. Brandt<br/>
+	    Owen Williams<br/>
+	    Bill Good<br/>
+	    Vittorio Colao<br/>
+	  </p>
+
+	  <p class="credits"><b>With contributions from:</b></p>
+	  <p class="credits">
+	    Mark Hills<br/>
+	    Andre Roth<br/>
+	    Robin Sheat<br/>
+	    Mark Glines<br/>
+	    Mathieu Rene<br/>
+	    Miko Kiiski<br/>
+	    Brian Jackson<br/>
+	    Andreas Pflug<br/>
+	    Bas van Schaik<br/>
+	    J&aacute;n Jockusch<br/>
+	    Oliver St&ouml;neberg<br/>
+	    Jan Jockusch<br/>
+	    C. Stewart<br/>
+	    Bill Egert<br/>
+	    Zach Shutters<br/>
+	    Owen Bullock<br/>
+	    Graeme Mathieson<br/>
+	    Sebastian Actist<br/>
+	    Jussi Sainio<br/>
+	    David Gnedt<br/>
+	    Antonio Passamani<br/>
+	    Guy Martin<br/>
+	    Anders Gunnarsson<br/>
+	    Alex Barker<br/>
+	    Mikko Jania<br/>
+	    Juan Pedro Bol&iacute;var Puente<br/>
+	    Linus Amvall<br/>
+	    Irwin C&eacute;spedes B<br/>
+	    Micz Flor<br/>
+	    Daniel James<br/>
+	    Mika Haulo<br/>
+	    Till Hofmann<br/> 
+	    Matthew Mikolay<br/>
+	    Tom Mast<br/>
+	    Bruno Buccolo<br/>
+	    Ryan Baker<br/>
+	    Miko Kiiski<br/>
+	    Vin&iacute;cius Dias dos Santos<br/>
+	    Joe Colosimo<br/>
+	    Shashank Kumar<br/>
+	    Daniel Sch&uuml;rmann<br/>
+	    Peter V&aacute;gner<br/>
+	    Thanasis Liappis<br/>
+	    Jens Nachtigall<br/>
+	  </p>
+
+	  <p class="credits"><b>And special thanks to:</b></p>
+	  <p class="credits">
+	    <a href="http://www.stantondj.com">Stanton</a><br/>
+	    <a href="http://www.hercules.com">Hercules</a><br/>
+	    <a href="http://www.vestax.com">Vestax</a><br/>
+	    <a href="http://www.echoaudio.com">Echo Digital Audio</a><br/>
+	    <a href="http://www.eks.fi">EKS</a><br/>
+	    <a href="http://code.google.com/soc">Google Summer of Code</a><br/>
+	    Adam Bellinson<br/>
+	    Alexandre Bancel<br/>
+	    Melanie Thielker<br/>
+	    Julien Rosener<br/>
+	    Pau Arum&iacute;<br/>
+	    David Garcia<br/>
+	    Seb Ruiz<br/>
+	    Joseph Mattiello<br/>
+	  </p>
 
 
-<p class="credits"><b>Past Developers</b></p>
-<p class="credits">
-<a href="http://haste.dk/tue/">Tue Haste Andersen</a><br>
-Ken Haste Andersen<br>
-Cedric Gestes<br>
-John Sully<br>
-Torben Hohn<br>
-Peter Chang<br>
-Micah Lee<br>
-Ben Wheeler<br>
-Wesley Stessens<br>
-Nathan Prado<br>
-Zach Elko<br>
-Tom Care<br>
-Pawel Bartkiewicz<br>
-<a href="http://kousu.ca/">Nick Guenther</a><br>
-</p>
+	  <p class="credits"><b>Past Developers</b></p>
+	  <p class="credits">
+	    <a href="http://haste.dk/tue/">Tue Haste Andersen</a><br/>
+	    Ken Haste Andersen<br/>
+	    Cedric Gestes<br/>
+	    John Sully<br/>
+	    Torben Hohn<br/>
+	    Peter Chang<br/>
+	    Micah Lee<br/>
+	    Ben Wheeler<br/>
+	    Wesley Stessens<br/>
+	    Nathan Prado<br/>
+	    Zach Elko<br/>
+	    Tom Care<br/>
+	    Pawel Bartkiewicz<br/>
+	    <a href="http://kousu.ca/">Nick Guenther</a><br/>
+	  </p>
 
-<p class="credits"><b>Past Contributors</b></p>
-<p class="credits">
-Ludek Hor&#225;cek<br>
-Svein Magne Bang<br>
-Kristoffer Jensen<br>
-Ingo Kossyk<br>
-Mads Holm<br>
-Lukas Zapletal<br>
-Jeremie Zimmermann<br>
-Gianluca Romanin<br>
-Tim Jackson<br>
-Stefan Langhammer<br>
-Frank Willascheck<br>
-Jeff Nelson<br>
-Kevin Schaper<br>
-Alex Markley<br>
-Oriol Puigb&oacute;<br>
-Ulrich Heske<br>
-James Hagerman<br>
-quil0m80<br>
-Martin Sakm&#225;r<br>
-Ilian Persson<br>
-Alex Barker<br>
-Dave Jarvis<br>
-Thomas Baag<br>
-Karlis Kalnins<br>
-Amias Channer<br>
-Sacha Berger<br>
-James Evans<br>
-Martin Sakmar<br>
-Navaho Gunleg<br>
-Gavin Pryke<br>
-Michael Pujos<br>
-Claudio Bantaloukas<br>
-Pavol Rusnak<br>
-</p>
+	  <p class="credits"><b>Past Contributors</b></p>
+	  <p class="credits">
+	    Ludek Hor&#225;cek<br/>
+	    Svein Magne Bang<br/>
+	    Kristoffer Jensen<br/>
+	    Ingo Kossyk<br/>
+	    Mads Holm<br/>
+	    Lukas Zapletal<br/>
+	    Jeremie Zimmermann<br/>
+	    Gianluca Romanin<br/>
+	    Tim Jackson<br/>
+	    Stefan Langhammer<br/>
+	    Frank Willascheck<br/>
+	    Jeff Nelson<br/>
+	    Kevin Schaper<br/>
+	    Alex Markley<br/>
+	    Oriol Puigb&oacute;<br/>
+	    Ulrich Heske<br/>
+	    James Hagerman<br/>
+	    quil0m80<br/>
+	    Martin Sakm&#225;r<br/>
+	    Ilian Persson<br/>
+	    Alex Barker<br/>
+	    Dave Jarvis<br/>
+	    Thomas Baag<br/>
+	    Karlis Kalnins<br/>
+	    Amias Channer<br/>
+	    Sacha Berger<br/>
+	    James Evans<br/>
+	    Martin Sakmar<br/>
+	    Navaho Gunleg<br/>
+	    Gavin Pryke<br/>
+	    Michael Pujos<br/>
+	    Claudio Bantaloukas<br/>
+	    Pavol Rusnak<br/>
+	  </p>
 
-		<p align="center">Mixxx also depends on a number of software libraries. Without these, Mixxx would not be possible, especially as a cross-platform application. Big thanks to <a href="http://qt.nokia.com">Qt</a>, <a href="http://www.portaudio.com">PortAudio</a>, and <a href="http://www.cs.cmu.edu/~music/portmusic/">PortMidi</a> for being fantastic.
-		<br>
-		<br><a href="images/splash_4.png">"Superstar DJs"</a> splash image uses source material licensed under
-		 Creative Commons Attribution 3.0 Unported from Google Picasa users 
-		 <a href="http://picasaweb.google.com/goatstrumpet/DJs#5382032223720336050">Goat's Trumpet</a> and
-		 <a href="http://picasaweb.google.com/organza.silverwing/RLVOE#5252292050442065426">Organza</a>, and
-		 Flickr user <a href="http://www.flickr.com/photos/ubefoto/4091198318/">Ubefoto</a>,
-		 accessed on August 15, 2010. 
-		 <br>
-		 <br>Lastly, we'd like to thank the many musicians and DJs that have helped in designing Mixxx.
-		 </p>
-    		</div>
+	  <p align="center">
+	    Mixxx also depends on a number of software libraries. Without
+	    these, Mixxx would not be possible, especially as a cross-platform
+	    application. Big thanks
+	    to <a href="http://qt.nokia.com">Qt</a>, <a href="http://www.portaudio.com">PortAudio</a>,
+	    and <a href="http://www.cs.cmu.edu/~music/portmusic/">PortMidi</a>
+	    for being fantastic.
+
+	    <br/><br/>
+
+	    <a href="images/splash_4.png">"Superstar DJs"</a> splash image uses
+	    source material licensed under Creative Commons Attribution 3.0
+	    Unported from Google Picasa users
+	    <a href="http://picasaweb.google.com/goatstrumpet/DJs#5382032223720336050">Goat's Trumpet</a> and
+	    <a href="http://picasaweb.google.com/organza.silverwing/RLVOE#5252292050442065426">Organza</a>, and
+	    Flickr user <a href="http://www.flickr.com/photos/ubefoto/4091198318/">Ubefoto</a>,
+	    accessed on August 15, 2010. 
+
+	    <br/><br/>
+	    Lastly, we'd like to thank the many musicians and DJs that have helped in designing Mixxx.
+	  </p>
+    	</div>
+      </div>
     </div>
-    </div>
-
+    
     <!-- ##### Footer ##### -->
-	<?php include 'footer.php' ?>
-	
+    <?php include 'footer.php' ?>
+    
   </body>
 </html>
