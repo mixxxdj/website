95eeeeda5313f0b710e36f9efae75ec046d1a9a7
RJ Ryan <rryan@mixxx.org>
2012-06-29 20:37:35 -0400

    Add IRC link using Freenode web client.

---
+++ irc.php
@@ -0,0 +1,17 @@
+<?php 
+$title = "Support";
+$meta = "Download the most advanced FREE DJ software available, featuring iTunes integration, MIDI controller support, internet broadcasting, and integrated music library.";
+
+require('header.php'); 
+?>
+<?php
+require('navbar.php'); 
+?>
+
+<div id="wrapper">
+
+<div class="content">
+<H1>IRC Channel</H1>
+<iframe src="http://webchat.freenode.net?channels=mixxx&uio=MTE9MjEa0" width="647" height="400"></iframe>
+
+<?php require('footer.php'); ?>
