4cf82d96c6171b0f025c89d44118bab24e0bcae2
jus <s.brandt@mixxx.org>
2013-04-30 14:03:37 +0200

    Add some missing <br/> and change tab->spaces in contact.php

--- contact.php
+++ contact.php
@@ -15,228 +15,228 @@ require('navbar.php');
 
 <div style="clear: both;"></div>
 <div class="halfbox_left">
-	  <p>
-	    Support for <b>users</b> of Mixxx is available through
-	    our <a href="/forums">community forums</a> and our IRC channel as
-	    described on our <a href="/support">Support page</a>.
+      <p>
+        Support for <b>users</b> of Mixxx is available through
+        our <a href="/forums">community forums</a> and our IRC channel as
+        described on our <a href="/support">Support page</a>.
 
-	    If you think you've found a bug, please file a bug report in
-	    our <a href="https://bugs.launchpad.net/mixxx/">bug
-	      tracker</a>.
-	  </p>
+        If you think you've found a bug, please file a bug report in
+        our <a href="https://bugs.launchpad.net/mixxx/">bug
+          tracker</a>.
+      </p>
 
-	  <p>
-	    <b>Developers</b> working on Mixxx collaborate with each other via
-	    the <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">mixxx-devel</a>
-	    mailing list and our IRC channel (#mixxx on Freenode). To send an
-	    email to the list, you
-	    must <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">subscribe
-	      first</a>. The list is low traffic.
-	  </p>
+      <p>
+        <b>Developers</b> working on Mixxx collaborate with each other via
+        the <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">mixxx-devel</a>
+        mailing list and our IRC channel (#mixxx on Freenode). To send an
+        email to the list, you
+        must <a href="http://lists.sourceforge.net/lists/listinfo/mixxx-devel">subscribe
+          first</a>. The list is low traffic.
+      </p>
 
 </div>
 <div class="halfbox_right">
-	  <p>
-	    If you're a <b>DJ hardware manufacturer</b> and would like your
-	    product to work with Mixxx, please contact our Project
-	    Manager, <a href="http://www.hep.ucl.ac.uk/~adamd">Adam
-	      Davison</a>. We accept donations of DJ equipment which we use to
-	    improve both specific and general hardware support in Mixxx.
-	  </p>
+      <p>
+        If you're a <b>DJ hardware manufacturer</b> and would like your
+        product to work with Mixxx, please contact our Project
+        Manager, <a href="http://www.hep.ucl.ac.uk/~adamd">Adam
+          Davison</a>. We accept donations of DJ equipment which we use to
+        improve both specific and general hardware support in Mixxx.
+      </p>
 
-	  <p>
-	    <b>For other inquiries</b>, please
-	    contact <a href="http://www.hep.ucl.ac.uk/~adamd">Adam
-	      Davison</a>.
-	  </p>
+      <p>
+        <b>For other inquiries</b>, please
+        contact <a href="http://www.hep.ucl.ac.uk/~adamd">Adam
+          Davison</a>.
+      </p>
 </div>
 <div style="clear: both;"></div>
 
 <div style="text-align: center;">
-	  <h2>Credits</h2>
-	  <p>
-	    In 2001, Mixxx was started
-	    by <a href="http://www.nbi.dk/~kenand/">Ken</a>
-	    and <a href="http://haste.dk/tue/">Tue</a> Haste Andersen. Today,
-	    development is done by an open source development team from around
-	    the world, led by Adam Davison (Project Manager), RJ Ryan (Lead
-	    Developer), and S. Brandt (Lead Artist).
-	  </p>
+      <h2>Credits</h2>
+      <p>
+        In 2001, Mixxx was started
+        by <a href="http://www.nbi.dk/~kenand/">Ken</a>
+        and <a href="http://haste.dk/tue/">Tue</a> Haste Andersen. Today,
+        development is done by an open source development team from around
+        the world, led by Adam Davison (Project Manager), RJ Ryan (Lead
+        Developer), and S. Brandt (Lead Artist).
+      </p>
 
-	  <p class="credits"><b>Mixxx 1.11.0 Development Team</b></p>
+      <p class="credits"><b>Mixxx 1.11.0 Development Team</b></p>
 
-	  <p class="credits">
-	    <a href="http://www.santoni.ca/albert">Albert Santoni</a><br/>
-	    <a href="http://www.rustyryan.net">RJ Ryan</a><br/>
-	    <a href="http://www.seanpappalardo.com">Sean Pappalardo</a><br/>
-	    Phillip Whelan<br/>
-	    Tobias Rafreider<br/>
-	    S. Brandt<br/>
-            Bill Good<br/>
-	    Owen Williams<br/>
-	    Vittorio Colao<br/>
-	    Daniel Sch&uuml;rmann<br/>
-            Thomas Vincent<br/>
-            Ilkka Tuohela<br/>
-            Max Linke<br/>
-	  </p>
+      <p class="credits">
+        <a href="http://www.santoni.ca/albert">Albert Santoni</a><br/>
+        <a href="http://www.rustyryan.net">RJ Ryan</a><br/>
+        <a href="http://www.seanpappalardo.com">Sean Pappalardo</a><br/>
+        Phillip Whelan<br/>
+        Tobias Rafreider<br/>
+        S. Brandt<br/>
+        Bill Good<br/>
+        Owen Williams<br/>
+        Vittorio Colao<br/>
+        Daniel Sch&uuml;rmann<br/>
+        Thomas Vincent<br/>
+        Ilkka Tuohela<br/>
+        Max Linke<br/>
+      </p>
 
-	  <p class="credits"><b>With contributions from:</b></p>
-	  <p class="credits">
-	    Mark Hills<br/>
-	    Andre Roth<br/>
-	    Robin Sheat<br/>
-	    Mark Glines<br/>
-	    Mathieu Rene<br/>
-	    Miko Kiiski<br/>
-	    Brian Jackson<br/>
-	    Andreas Pflug<br/>
-	    Bas van Schaik<br/>
-	    J&aacute;n Jockusch<br/>
-	    Oliver St&ouml;neberg<br/>
-	    Jan Jockusch<br/>
-	    C. Stewart<br/>
-	    Bill Egert<br/>
-	    Zach Shutters<br/>
-	    Owen Bullock<br/>
-	    Graeme Mathieson<br/>
-	    Sebastian Actist<br/>
-	    Jussi Sainio<br/>
-	    David Gnedt<br/>
-	    Antonio Passamani<br/>
-	    Guy Martin<br/>
-	    Anders Gunnarsson<br/>
-	    Alex Barker<br/>
-	    Mikko Jania<br/>
-	    Juan Pedro Bol&iacute;var Puente<br/>
-	    Linus Amvall<br/>
-	    Irwin C&eacute;spedes B<br/>
-	    Micz Flor<br/>
-	    Daniel James<br/>
-	    Mika Haulo<br/>
-	    Till Hofmann<br/>
-	    Matthew Mikolay<br/>
-	    Tom Mast<br/>
-	    Bruno Buccolo<br/>
-	    Ryan Baker<br/>
-	    Miko Kiiski<br/>
-	    Vin&iacute;cius Dias dos Santos<br/>
-	    Joe Colosimo<br/>
-	    Shashank Kumar<br/>
-	    Daniel Sch&uuml;rmann<br/>
-	    Peter V&aacute;gner<br/>
-	    Thanasis Liappis<br/>
-	    Jens Nachtigall<br/>
-	    Scott Ullrich<br/>
-	    Jonas &Aring;dahl<br/>
-            Jonathan Costers<br/>
-            Daniel Lindenfelser<br/>
-	    Maxime Bochon<br/>
-	    Akash Shetye
-            Pascal Bleser<br/>
-            Florian Mahlknecht<br/>
-	    Ben Clark
-            Tom Gascoigne<br/>
-            Neale Pickett<br/>
-            Aaron Mavrinac<br/>
-            Markus H&auml;rer<br/>
-            Andrey Smelov<br/>
-            Scott Stewart<br/>
-            Nimatek<br/>
-            Alban Bedel<br/>
-            Steven Boswell<br/>
-            Jo&atilde;o Reys Santos<br/>
-	  </p>
+      <p class="credits"><b>With contributions from:</b></p>
+      <p class="credits">
+        Mark Hills<br/>
+        Andre Roth<br/>
+        Robin Sheat<br/>
+        Mark Glines<br/>
+        Mathieu Rene<br/>
+        Miko Kiiski<br/>
+        Brian Jackson<br/>
+        Andreas Pflug<br/>
+        Bas van Schaik<br/>
+        J&aacute;n Jockusch<br/>
+        Oliver St&ouml;neberg<br/>
+        Jan Jockusch<br/>
+        C. Stewart<br/>
+        Bill Egert<br/>
+        Zach Shutters<br/>
+        Owen Bullock<br/>
+        Graeme Mathieson<br/>
+        Sebastian Actist<br/>
+        Jussi Sainio<br/>
+        David Gnedt<br/>
+        Antonio Passamani<br/>
+        Guy Martin<br/>
+        Anders Gunnarsson<br/>
+        Alex Barker<br/>
+        Mikko Jania<br/>
+        Juan Pedro Bol&iacute;var Puente<br/>
+        Linus Amvall<br/>
+        Irwin C&eacute;spedes B<br/>
+        Micz Flor<br/>
+        Daniel James<br/>
+        Mika Haulo<br/>
+        Till Hofmann<br/>
+        Matthew Mikolay<br/>
+        Tom Mast<br/>
+        Bruno Buccolo<br/>
+        Ryan Baker<br/>
+        Miko Kiiski<br/>
+        Vin&iacute;cius Dias dos Santos<br/>
+        Joe Colosimo<br/>
+        Shashank Kumar<br/>
+        Daniel Sch&uuml;rmann<br/>
+        Peter V&aacute;gner<br/>
+        Thanasis Liappis<br/>
+        Jens Nachtigall<br/>
+        Scott Ullrich<br/>
+        Jonas &Aring;dahl<br/>
+        Jonathan Costers<br/>
+        Daniel Lindenfelser<br/>
+        Maxime Bochon<br/>
+        Akash Shetye<br/>
+        Pascal Bleser<br/>
+        Florian Mahlknecht<br/>
+        Ben Clark<br/>
+        Tom Gascoigne<br/>
+        Neale Pickett<br/>
+        Aaron Mavrinac<br/>
+        Markus H&auml;rer<br/>
+        Andrey Smelov<br/>
+        Scott Stewart<br/>
+        Nimatek<br/>
+        Alban Bedel<br/>
+        Steven Boswell<br/>
+        Jo&atilde;o Reys Santos<br/>
+      </p>
 
-	  <p class="credits"><b>And special thanks to:</b></p>
-	  <p class="credits">
-            <a href="http://www.vestax.com">Vestax</a><br/>
-	    <a href="http://www.stantondj.com">Stanton</a><br/>
-	    <a href="http://www.hercules.com">Hercules</a><br/>
-	    <a href="http://www.eks.fi">EKS</a><br/>
-            <a href="http://www.echoaudio.com">Echo Digital Audio</a><br/>
-            JP Disco<br/>
-	    <a href="http://code.google.com/soc">Google Summer of Code</a><br/>
-	    Adam Bellinson<br/>
-	    Alexandre Bancel<br/>
-	    Melanie Thielker<br/>
-	    Julien Rosener<br/>
-	    Pau Arum&iacute;<br/>
-	    David Garcia<br/>
-	    Seb Ruiz<br/>
-	    Joseph Mattiello<br/>
-	  </p>
+      <p class="credits"><b>And special thanks to:</b></p>
+      <p class="credits">
+        <a href="http://www.vestax.com">Vestax</a><br/>
+        <a href="http://www.stantondj.com">Stanton</a><br/>
+        <a href="http://www.hercules.com">Hercules</a><br/>
+        <a href="http://www.eks.fi">EKS</a><br/>
+        <a href="http://www.echoaudio.com">Echo Digital Audio</a><br/>
+        JP Disco<br/>
+        <a href="http://code.google.com/soc">Google Summer of Code</a><br/>
+        Adam Bellinson<br/>
+        Alexandre Bancel<br/>
+        Melanie Thielker<br/>
+        Julien Rosener<br/>
+        Pau Arum&iacute;<br/>
+        David Garcia<br/>
+        Seb Ruiz<br/>
+        Joseph Mattiello<br/>
+      </p>
 
 
-	  <p class="credits"><b>Past Developers</b></p>
-	  <p class="credits">
-	    <a href="http://haste.dk/tue/">Tue Haste Andersen</a><br/>
-	    Ken Haste Andersen<br/>
-	    Cedric Gestes<br/>
-	    John Sully<br/>
-	    Torben Hohn<br/>
-	    Peter Chang<br/>
-	    Micah Lee<br/>
-	    Ben Wheeler<br/>
-	    Wesley Stessens<br/>
-	    Nathan Prado<br/>
-	    Zach Elko<br/>
-	    Tom Care<br/>
-	    Pawel Bartkiewicz<br/>
-	    <a href="http://kousu.ca/">Nick Guenther</a><br/>
-            <a href="http://www.hep.ucl.ac.uk/~adamd">Adam Davison</a><br/>
-	    <a href="http://stacktrace.org/">Garth Dahlstrom</a><br/>
-	  </p>
+      <p class="credits"><b>Past Developers</b></p>
+      <p class="credits">
+        <a href="http://haste.dk/tue/">Tue Haste Andersen</a><br/>
+        Ken Haste Andersen<br/>
+        Cedric Gestes<br/>
+        John Sully<br/>
+        Torben Hohn<br/>
+        Peter Chang<br/>
+        Micah Lee<br/>
+        Ben Wheeler<br/>
+        Wesley Stessens<br/>
+        Nathan Prado<br/>
+        Zach Elko<br/>
+        Tom Care<br/>
+        Pawel Bartkiewicz<br/>
+        <a href="http://kousu.ca/">Nick Guenther</a><br/>
+        <a href="http://www.hep.ucl.ac.uk/~adamd">Adam Davison</a><br/>
+        <a href="http://stacktrace.org/">Garth Dahlstrom</a><br/>
+      </p>
 
-	  <p class="credits"><b>Past Contributors</b></p>
-	  <p class="credits">
-	    Ludek Hor&#225;cek<br/>
-	    Svein Magne Bang<br/>
-	    Kristoffer Jensen<br/>
-	    Ingo Kossyk<br/>
-	    Mads Holm<br/>
-	    Lukas Zapletal<br/>
-	    Jeremie Zimmermann<br/>
-	    Gianluca Romanin<br/>
-	    Tim Jackson<br/>
-	    Stefan Langhammer<br/>
-	    Frank Willascheck<br/>
-	    Jeff Nelson<br/>
-	    Kevin Schaper<br/>
-	    Alex Markley<br/>
-	    Oriol Puigb&oacute;<br/>
-	    Ulrich Heske<br/>
-	    James Hagerman<br/>
-	    quil0m80<br/>
-	    Martin Sakm&#225;r<br/>
-	    Ilian Persson<br/>
-	    Alex Barker<br/>
-	    Dave Jarvis<br/>
-	    Thomas Baag<br/>
-	    Karlis Kalnins<br/>
-	    Amias Channer<br/>
-	    Sacha Berger<br/>
-	    James Evans<br/>
-	    Martin Sakmar<br/>
-	    Navaho Gunleg<br/>
-	    Gavin Pryke<br/>
-	    Michael Pujos<br/>
-	    Claudio Bantaloukas<br/>
-	    Pavol Rusnak<br/>
-            Bruno Buccolo<br/>
-            Ryan Baker<br/>
-	  </p>
+      <p class="credits"><b>Past Contributors</b></p>
+      <p class="credits">
+        Ludek Hor&#225;cek<br/>
+        Svein Magne Bang<br/>
+        Kristoffer Jensen<br/>
+        Ingo Kossyk<br/>
+        Mads Holm<br/>
+        Lukas Zapletal<br/>
+        Jeremie Zimmermann<br/>
+        Gianluca Romanin<br/>
+        Tim Jackson<br/>
+        Stefan Langhammer<br/>
+        Frank Willascheck<br/>
+        Jeff Nelson<br/>
+        Kevin Schaper<br/>
+        Alex Markley<br/>
+        Oriol Puigb&oacute;<br/>
+        Ulrich Heske<br/>
+        James Hagerman<br/>
+        quil0m80<br/>
+        Martin Sakm&#225;r<br/>
+        Ilian Persson<br/>
+        Alex Barker<br/>
+        Dave Jarvis<br/>
+        Thomas Baag<br/>
+        Karlis Kalnins<br/>
+        Amias Channer<br/>
+        Sacha Berger<br/>
+        James Evans<br/>
+        Martin Sakmar<br/>
+        Navaho Gunleg<br/>
+        Gavin Pryke<br/>
+        Michael Pujos<br/>
+        Claudio Bantaloukas<br/>
+        Pavol Rusnak<br/>
+        Bruno Buccolo<br/>
+        Ryan Baker<br/>
+      </p>
  </div>
 
-	  <p align="center">
-	    Mixxx also depends on a number of software libraries. Without
-	    these, Mixxx would not be possible, especially as a cross-platform
-	    application. Big thanks
-	    to <a href="http://qt.nokia.com">Qt</a>, <a href="http://www.portaudio.com">PortAudio</a>,
-	    and <a href="http://www.cs.cmu.edu/~music/portmusic/">PortMidi</a>
-	    for being fantastic.
+      <p align="center">
+        Mixxx also depends on a number of software libraries. Without
+        these, Mixxx would not be possible, especially as a cross-platform
+        application. Big thanks
+        to <a href="http://qt.nokia.com">Qt</a>, <a href="http://www.portaudio.com">PortAudio</a>,
+        and <a href="http://www.cs.cmu.edu/~music/portmusic/">PortMidi</a>
+        for being fantastic.
 
-	    <br/><br/>
-	    Lastly, we'd like to thank the many musicians and DJs that have helped in designing Mixxx.
+        <br/><br/>
+        Lastly, we'd like to thank the many musicians and DJs that have helped in designing Mixxx.
 
 <?php require('footer.php'); ?>
