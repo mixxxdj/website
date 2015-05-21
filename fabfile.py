87967f9e12edc58b3aec7372ba0214d9978da1d2
RJ Ryan <rryan@mixxx.org>
2015-05-21 14:42:50 -0400

    Fabfile updates for i18n.

--- fabfile.py
+++ fabfile.py
@@ -1,49 +1,66 @@
 import os
 
 from fabric.api import *
-from fabric.state import env
 import fabric.contrib.project as project
 
-STAGING_HOST = 'stacktrace.org'
-STAGING_DEST_PATH = '/home/mixxx/domains/staging.mixxx.org/public_html'
-PROD_HOST = 'stacktrace.org'
-PROD_DEST_PATH = '/home/mixxx/public_html'
+LANGUAGE_CONFIGS = {
+    'en': { 'dest_path': '/' },
+    'de': { 'dest_path': '/de' },
+    'de-DE': { 'dest_path': '/de-DE' }
+}
+
+env.roledefs = {
+    'production': {
+        'hosts': ['stacktrace.org'],
+        'dest_path': '/home/mixxx/public_html',
+    },
+    'staging': {
+        'hosts': ['stacktrace.org'],
+        'dest_path': '/home/mixxx/domains/staging.mixxx.org/public_html',
+    }
+}
 
 env.deploy_path = '.build'
 DEPLOY_PATH = env.deploy_path
-
 env.user = 'mixxx'
+
+def staging():
+    env.roles = ['staging']
+    env.dest_path = env.roledefs['staging']['dest_path']
+
+def production():
+    env.roles = ['production']
+    env.dest_path = env.roledefs['production']['dest_path']
 
 def clean():
     if os.path.isdir(DEPLOY_PATH):
         local('rm -rf {deploy_path}'.format(**env))
         local('mkdir {deploy_path}'.format(**env))
 
-def build():
-    local('cactus build')
+def build(language='en'):
+    config = LANGUAGE_CONFIGS.get(language, None)
+    if config is None:
+        raise Exception('Invalid language: %s' % language)
+    config_file = 'config.%s.json' % language if language != 'en' else 'config.json'
+    local('cactus build -c %s' % config_file)
+    if 'dest_path' in env:
+        env.dest_path = env.dest_path + config['dest_path']
 
-def rebuild():
+def rebuild(*args, **kwargs):
     clean()
-    build()
+    build(*args, **kwargs)
 
-def serve():
+def serve(language='en'):
     clean()
-    local('cactus serve')
+    config = LANGUAGE_CONFIGS.get(language, None)
+    if config is None:
+        raise Exception('Invalid language: %s' % language)
+    config_file = 'config.%s.json' % language if language != 'en' else 'config.json'
+    local('cactus serve -c %s' % config_file)
 
-@hosts(PROD_HOST)
 def publish():
     project.rsync_project(
-        remote_dir=PROD_DEST_PATH,
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        exclude=('.DS_Store', '.git'),
-        delete=False,
-        extra_opts='-c',
-    )
-
-@hosts(STAGING_HOST)
-def staging():
-    project.rsync_project(
-        remote_dir=STAGING_DEST_PATH,
+        remote_dir=env.dest_path,
         local_dir=DEPLOY_PATH.rstrip('/') + '/',
         exclude=('.DS_Store', '.git'),
         delete=False,
