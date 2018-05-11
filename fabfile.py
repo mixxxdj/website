22dc1349ab2ed88cc185a3653f8d9b8fc8861fbe
Sean M. Pappalardo <spappalardo@mixxx.org>
2018-05-11 13:15:53 -0700

    Escape quotes

--- fabfile.py
+++ fabfile.py
@@ -113,7 +113,7 @@ def snapshot(snapshot_name=None):
         wiki_path = os.path.join(instance_path, 'wiki')
         wiki_snapshot = 'wiki.tar.bz2'
         # Exclusions to save space & time per https://www.dokuwiki.org/faq:backup
-        sudo('tar --exclude='data/cache' --exclude='data/index' --exclude='data/locks' --exclude='data/tmp' -cjpf {} -C {} .'.format(wiki_snapshot, wiki_path))
+        sudo('tar --exclude=\'data/cache\' --exclude=\'data/index\' --exclude=\'data/locks\' --exclude=\'data/tmp\' -cjpf {} -C {} .'.format(wiki_snapshot, wiki_path))
 
     # Record snapshot name for chaining commands.
     env.snapshot_name = snapshot_name
