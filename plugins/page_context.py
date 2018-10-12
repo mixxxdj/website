743d41124febd4d46e014ab9ea392cde63f0aff1
Radu Suciu <radusuciu@gmail.com>
2018-10-12 12:52:20 -0700

    Correct links

--- plugins/page_context.py
+++ plugins/page_context.py
@@ -9,16 +9,16 @@ def preBuildPage(page, context, data):
     # Any changes you make to context will be passed to the template renderer for this page.
 
     menu = [
-        ("/download.html", "big", "Download", "Navigation bar link to Mixxx download page."),
-        ("/features.html", "big", "Features", "Navigation bar link to Mixxx features page."),
-        ("/support.html", "big", "Support & Community", "Navigation bar link to Mixxx support page."),
+        ("/download", "big", "Download", "Navigation bar link to Mixxx download page."),
+        ("/features", "big", "Features", "Navigation bar link to Mixxx features page."),
+        ("/support", "big", "Support & Community", "Navigation bar link to Mixxx support page."),
         ("/manual/latest", "medium", "Manual", "Navigation bar link to Mixxx Manual."),
         ("/forums", "medium", "Forums", "Navigation bar link to Mixxx Forums."),
         ("/wiki", "medium", "Wiki", "Navigation bar link to Mixxx Wiki."),
         ("http://mixxxblog.blogspot.com", "medium", "Blog", "Navigation bar link to Mixxx blog."),
-        ("/press.html", "small", "Press", "Navigation bar link to Mixxx Press page"),
-        ("/get-involved.html", "small", "Get Involved", "Navigation bar link to Mixxx Get Involved page." ),
-        ("/contact.html", "small", "Contact", "Navigation bar link to Mixxx contact page.")
+        ("/press", "small", "Press", "Navigation bar link to Mixxx Press page"),
+        ("/get-involved", "small", "Get Involved", "Navigation bar link to Mixxx Get Involved page." ),
+        ("/contact", "small", "Contact", "Navigation bar link to Mixxx contact page.")
     ]
 
     extra = {
