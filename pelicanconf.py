1a7d82bbb6aff6f85ba2d3022a75f726c9be4e9b
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-12-13 22:15:53 +0100

    Pelican: Remove unused stuff from config file

--- pelicanconf.py
+++ pelicanconf.py
@@ -236,18 +236,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+LINKS = ()
 
 # Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = 10
 DELETE_OUTPUT_DIRECTORY = True
