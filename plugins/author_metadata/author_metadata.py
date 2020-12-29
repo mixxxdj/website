afa53b681db64fc7a1a38e5057cdcc2656816da3
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-12-30 00:34:16 +0100

    Pelican: Add support for fetching missing author avatars automatically

--- plugins/author_metadata/author_metadata.py
+++ plugins/author_metadata/author_metadata.py
@@ -1,8 +1,25 @@
+import logging
+import os
+import urllib.error
+import urllib.request
+
 from pelican import signals
+from pelican.utils import mkdir_p, get_relative_path, path_to_url
+
+logger = logging.getLogger(__name__)
 
 
-def update_author(settings, author):
+def download_avatar(username):
+    url = f"https://avatars.githubusercontent.com/{username}?size=60"
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+    return data
+
+
+def update_author(article_generator, author):
     """ Updates the Author metadata objects with extra information. """
+    settings = article_generator.settings
+
     author_metadata = settings.get("AUTHOR_METADATA", {})
     author_dict = author_metadata.get(author.name, {})
 
@@ -31,6 +48,34 @@ def update_author(settings, author):
     )
     author.email = author_dict.get("email")
 
+    author.avatar_url = None
+    if not author.github or "images" not in settings["STATIC_PATHS"]:
+        return
+    path = os.path.join(article_generator.path, "images", "avatars")
+    avatar_path = os.path.join(path, f"{author.github}.png")
+    if not os.path.exists(avatar_path):
+        logger.warning(
+            "Missing avatar for author '%s', expected %s",
+            author.name,
+            avatar_path,
+        )
+        logger.debug(
+            "Downloading missing avatar for GitHub user %s...",
+            author.github,
+        )
+        try:
+            data = download_avatar(author.github)
+        except urllib.error.URLError:
+            logger.warning("Download of avatar failed, skipping...")
+            return
+        mkdir_p(path)
+        with open(avatar_path, "w+b") as fp:
+            fp.write(data)
+        logger.warning("Downloaded missing avatar to: %s", avatar_path)
+    author.avatar_url = path_to_url(
+        os.path.relpath(avatar_path, article_generator.path)
+    )
+
 
 def article_generator_context(article_generator, metadata):
     """
@@ -38,10 +83,10 @@ def article_generator_context(article_generator, metadata):
     properties.
     """
     for author in metadata.get("authors", []):
-        update_author(article_generator.settings, author)
+        update_author(article_generator, author)
 
     if "author" in metadata:
-        update_author(article_generator.settings, metadata["author"])
+        update_author(article_generator, metadata["author"])
 
 
 def register():
