cd48b0094b652bd6510d862e13aefac8c2cca4cf
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-04-17 17:11:52 +0200

    theme/static/js/discourse.js: Fix undefined variable warning
    Fixes:
    
        theme/static/js/discourse.js
          22:21  error  'DiscourseEmbed' is not defined  no-undef

--- theme/static/js/discourse.js
+++ theme/static/js/discourse.js
@@ -19,7 +19,7 @@
             var d = document.createElement("script");
             d.type = "text/javascript";
             d.async = true;
-            d.src = DiscourseEmbed.discourseUrl + "javascripts/embed.js";
+            d.src = window.DiscourseEmbed.discourseUrl + "javascripts/embed.js";
             (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(d);
         })();
     });
